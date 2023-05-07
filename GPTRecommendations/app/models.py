dde3f1e816eb5c0e06fa372b12f378edb34b640c
rmirzoev <78797322+rmirzoev@users.noreply.github.com>
2023-05-07 18:26:23 +0300

    Update models.py
    Добавлена модель для отслеживания посещений (TrackVisits)

--- GPTRecommendations/app/models.py
+++ GPTRecommendations/app/models.py
@@ -1,41 +1,44 @@
+from django.db import models
+from django.db import models
 import os
-
 from urllib import response
 import openai
+from configparser import ConfigParser
+
+from hitcount.models import HitCountMixin, HitCount
+
 openai.organization = "org-pbKlOp3rGztChAWCyGgJjxSQ"
 
-from configparser import ConfigParser
 config = ConfigParser()
-configFilePath = os.path.join(os.path.dirname(__file__), 'auth.ini');
+configFilePath = os.path.join(os.path.dirname(__file__), 'auth.ini')
 config.sections()
 config.read(configFilePath)
 YOUR_API_KEY = config.get('auth', 'API_KEY')
 openai.api_key = YOUR_API_KEY
 openai.Model.list()
 
-"""
-Definition of models.
-"""
-
-from django.db import models
-
 # Create your models here.
+
 
 class Music():
     music = str(models.CharField(max_length=100))
     artist = str(models.CharField(max_length=100))
+
     def __init__(self, music, artist):
-      self.music = music
-      self.artist = artist
+        self.music = music
+        self.artist = artist
+
 
 class Query(models.Model):
     genre = str(models.CharField(max_length=100))
     artist = str(models.CharField(max_length=100))
     question = str(models.CharField(max_length=300))
+
     def __init__(self, genre, question, artist):
-      self.genre = genre
-      self.question = question
-      self.artist = artist
+        self.genre = genre
+        self.question = question
+        self.artist = artist
+
 
 class QueryProcessor():
     query: Query
@@ -43,39 +46,47 @@ class QueryProcessor():
     response = ''
     answer = ''
     table = []
+
     def __init__(self, query):
-      self.query = query
+        self.query = query
 
     def process(self):
-
-        if (self.query.artist != None and self.query.genre != None):
-            self.prompt = '###Instruction### Recommend music in the'+ str(self.query.genre) +' genre that suits the description' + str(self.query.question) + ' and is made by' + str(self.query.artist) + '. Format the result as a table such as "TABLE: 1.1. first music name 1.2. first music artist name 2.1. second music name 2.2. second music artist name" etc. The table should have 9 rows.'
-        elif (self.query.artist == None and self.query.genre != None):
-             self.prompt = '###Instruction### Recommend music in the'+ str(self.query.genre) +' genre that suits the description' + str(self.query.question) + '. Format the result as a table such as "TABLE: 1.1. first music name 1.2. first music artist name 2.1. second music name 2.2. second music artist name" etc. The table should have 9 rows.'
-        elif (self.query.artist != None and self.query.genre == None):
-             self.prompt = '###Instruction### Recommend music that suits the description' + str(self.query.question) + ' and is made by' + str(self.query.artist) + '. Format the result as a table such as "TABLE: 1.1. first music name 1.2. first music artist name 2.1. second music name 2.2. second music artist name" etc. The table should have 9 rows.'
-        elif (self.query.artist == None and self.query.genre == None):
-             self.prompt = '###Instruction### Recommend music that suits the description' + str(self.query.question) + '. Format the result as a table such as "TABLE: 1.1. first music name 1.2. first music artist name 2.1. second music name 2.2. second music artist name" etc. The table should have 9 rows.'
+        if (self.query.artist is not None) and (self.query.genre is not None):
+            self.prompt = '###Instruction### Recommend music in the' + str(self.query.genre) + ' genre that suits the description' + str(self.query.question) + ' and is made by' + str(self.query.artist) + '. Format the result as a table such as "TABLE: 1.1. first music name 1.2. first music artist name 2.1. second music name 2.2. second music artist name" etc. The table should have 9 rows.'
+        elif (self.query.artist is None) and (self.query.genre is not None):
+            self.prompt = '###Instruction### Recommend music in the' + str(self.query.genre) + ' genre that suits the description' + str(self.query.question) + '. Format the result as a table such as "TABLE: 1.1. first music name 1.2. first music artist name 2.1. second music name 2.2. second music artist name" etc. The table should have 9 rows.'
+        elif (self.query.artist is not None) and (self.query.genre is None):
+            self.prompt = '###Instruction### Recommend music that suits the description' + str(self.query.question) + ' and is made by' + str(self.query.artist) + '. Format the result as a table such as "TABLE: 1.1. first music name 1.2. first music artist name 2.1. second music name 2.2. second music artist name" etc. The table should have 9 rows.'
+        elif (self.query.artist is None) and (self.query.genre is None):
+            self.prompt = '###Instruction### Recommend music that suits the description' + str(self.query.question) + '. Format the result as a table such as "TABLE: 1.1. first music name 1.2. first music artist name 2.1. second music name 2.2. second music artist name" etc. The table should have 9 rows.'
 
     def postRequest(self):
         self.response = openai.ChatCompletion.create(
-            model = "gpt-3.5-turbo", 
-            messages = [
+            model="gpt-3.5-turbo",
+            messages=[
                 {"role": "system", "content": "You are a helpful assistant."},
                 {"role": "user", "content": self.prompt}
-                ], 
-            temperature = 0.9, 
-            max_tokens = 400
+                ],
+            temperature=0.9,
+            max_tokens=400
             )
         self.answer = self.response['choices'][0]['message']['content']
 
     def parseRequest(self):
         self.table = []
-        for i in range (1, 9):
+        for i in range(1, 9):
             musicstart = self.answer.find(str(i)+'.1')
             musicend = self.answer.find(str(i)+'.2')
-            if (i != 9):
+            if i != 9:
                 artistend = self.answer.find(str(i+1)+'.1')
             else:
                 artistend = len(self.answer)-1
             self.table.append(Music(self.answer[musicstart:musicend], self.answer[musicend:artistend]))
+
+
+class TrackVisits(HitCountMixin, models.Model):
+
+    # method to get an amount of visits
+    def get_hit_count(self):
+        hit_count = HitCount.objects.get_for_object(self)
+        return hit_count.hits
