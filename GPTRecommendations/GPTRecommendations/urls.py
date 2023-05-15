6e6c4c92c20424d2a3347fe74f2540686547dca2
lagrangianFormalism <anastasia.navrotskaya@gmail.com>
2023-05-16 01:12:14 +0300

    попытка 2

--- GPTRecommendations/GPTRecommendations/urls.py
+++ GPTRecommendations/GPTRecommendations/urls.py
@@ -7,6 +7,7 @@ from django.urls import path
 from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 from app import forms, views
+from django.urls import path, include
 
 
 urlpatterns = [
